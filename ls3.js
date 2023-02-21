a1b4ea008a902075be315745f62a78e7d3949f28
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-02-21 13:31:20 +0500

    Added age

---
+++ ls3.js
@@ -0,0 +1,19 @@
+var yosh = 0 +prompt('Yoshingizni kiriting')
+
+if(yosh <= 18){
+    alert('Yoshsiz. Oqishingiz kk');
+}
+    else if(18 <= yosh && yosh <= 50){
+        alert('Ishlashingiz kerak');
+    }
+    else if(50 <= yosh && yosh <= 59){
+        alert('Yaqinda pensiyaga chiqasiz');
+    }
+    else if(59 <= yosh && yosh <= 150){
+        alert('Pensionerga oxshaysiz, hali tirik bolsangiz');
+    }
+    else
+        alert('Nimadir notogri ketkan')
+
+
+
